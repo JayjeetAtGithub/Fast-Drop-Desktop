c30448d8c181908c50615b1d5c72100129d1a868
Jayjeet Chakraborty <jc.github@rediffmail.com>
2019-11-16 21:30:26 +0530

    Remove sending capability and other fixes

--- app.py
+++ app.py
@@ -1,150 +1,108 @@
-#############################
-##### pyP2P 1.1 (alpha) #####
-#############################
-
 import socket
 import os
 import argparse
 import pyqrcode
 import netifaces
 
-
-class Logger:
-    """Color definations
-    """
-    OK = '\033[92m[+]\033[0m'
-    INFO = '\033[93m[!]\033[0m'
-    ERROR = '\033[91m[-]\033[0m'
-
-    def success(self, message):
-        print("{} {}".format(Logger.OK, message))
-
-    def info(self, message):
-        print("{} {}".format(Logger.INFO, message))
-
-    def error(self, message):
-        print("{} {}".format(Logger.ERROR, message))
-
-
-logger = Logger()
-
+downloads_dir = None
 
 def render_qr_code(addr):
-    """Generates a QR Code for the IP address and displays it on the terminal
+    """Generates a QR Code for the IP address and 
+    displays it on the terminal.
     """
     url = pyqrcode.create(str(addr))
     url.svg("addr.svg", scale=8)
     url.eps("addr.eps", scale=2)
     print(url.terminal(quiet_zone=1))
 
-
-def get_ip_address(ifname):
-    """Returns the ip address of the interface requested
-    """
-    return netifaces.ifaddresses(ifname)[netifaces.AF_INET][0]['addr']
-
-
 def create_downloads_folder():
-    """Creats a folder named `downloads` if not exists
+    """Checks if a `~/Downloads` dir is present. If not
+    it creates one.
     """
-    if not os.path.exists("downloads"):
-        os.mkdir("downloads")
-
+    downloads_dir = os.path.join(os.environ['HOME'], 'Downloads')
+    if not os.path.exists(downloads_dir):
+        os.mkdir(downloads_dir)
+    return downloads_dir
 
 def human(data):
     """Decodes byte string into human readable format
     """
     return data.decode("utf-8")
 
-
-def run(PORT=9000):
-    """Opens up a socket connection on the port requested
+def run(port=9000):
+    """Opens up a socket connection on the port requested.
+    If no value of port is supplied, port 9000 is used.
     """
+    host = get_ip()
+    print('Connect to {}:{}'.format(host, port))
+    print('Scan the QR Code to connect :)')
+    render_qr_code("{}:{}".format(host, port))
     try:
-        logger.success('Connect to {}:{}'.format(get_hotspot_ip(), PORT))
-        logger.info('Scan the QR Code to connect :)')
-        render_qr_code("{}:{}".format(get_hotspot_ip(), PORT))
-        handshake(PORT)
-    except (KeyboardInterrupt, Exception):
-        logger.error("Server stopped !")
+        connect_and_recv(host, port)
+    except (KeyboardInterrupt, Exception) as e:
+        print(e)
+        print("Server stopped !")
 
-
-def get_hotspot_ip():
-    """Gets the ip address of the hotspot
+def get_ip():
+    """Returns the IP address .
     """
-    return get_ip_address("wlp3s0")
+    return str(netifaces.ifaddresses("en0")[netifaces.AF_INET][0]['addr'])
 
-
-def handshake(PORT):
-    """Starts a socket server and waits to get connection and recieve files
+def connect_and_recv(host, port):
+    """Starts a socket server and waits 
+    for connection and recieve files.
     """
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        s.bind((str(get_hotspot_ip()), PORT))
+        s.bind((host, port))
         s.listen(1)
-    except OSError:
+    except Exception:
         s.close()
     while 1:
         conn, addr = s.accept()
-        logger.info("Connected by {}".format(addr))
-        dec = int(input("Receive or Send ? (1/2) "))
-        if dec == 1:
-            filename = str(input("Enter file name : "))
-            try:
-                receive_file(conn, filename)
-            except Exception:
-                s.close()
-        elif dec == 2:
-            path = str(input("Enter file path : "))
-            try:
-                send_file(conn, path)
-            except Exception:
-                s.close()
-        else:
-            logger.error("Wrong choice !")
+        try:
+            print("Connected by {}".format(addr))
+            filename = str(conn.recv(1024).decode('utf-8')).rstrip('\n')
+            print("Receiving : ", filename)
+        except Exception as e:
+            print("Could not recv filename !")              
+        finally:
+            conn.close()
 
+        conn, addr = s.accept()
+        try:
+            receive_file(conn, filename)
+        except Exception as e:
+            print("Could not recv file !")
+        finally:
+            conn.close()
 
 def receive_file(conn, filename):
-    """Recieves  a file byte by byte
+    """Recieves  a file byte by byte over
+    the connection.
     """
-    f = open("./downloads/{}".format(filename), 'wb')
+    f = open(os.path.join(downloads_dir, filename), 'wb')
     data = conn.recv(1024)
     while data:
         f.write(data)
         data = conn.recv(1024)
     f.close()
-    logger.success("File received successfully !")
-
-
-def send_file(conn, filepath):
-    """Sends a file over socket
-    """
-    if os.path.exists(filepath) and os.path.isfile(filepath):
-        f = open(filepath, 'rb')
-        data = f.read(1024)
-        while data:
-            conn.send(data)
-            data = f.read(1024)
-        f.close()
-        logger.success("File sent successfully !")
-    else:
-        logger.error("Invalid file path !")
+    print("File received successfully !")
 
 
 if __name__ == "__main__":
-
     art = """
-                     ____ ___   ____ 
-        ____  __  __/ __ \__ \ / __ )
-       / __ \/ / / / /_/ /_/ // /_/ /
-      / /_/ / /_/ / ____/ __// ____/ 
-     / .___/\__, /_/   /____/_/      
-    /_/    /____/                    
-
-
+           _        _____                  
+     /\   (_)      |  __ \                 
+    /  \   _ _ __  | |  | |_ __ ___  _ __  
+   / /\ \ | | '__| | |  | | '__/ _ \| '_ \ 
+  / ____ \| | |    | |__| | | | (_) | |_) |
+ /_/    \_\_|_|    |_____/|_|  \___/| .__/ 
+                                    | |    
+                                    |_|    
     """
     print("\033[93m {}\033[00m" .format(art))
-    create_downloads_folder()
+    downloads_dir = create_downloads_folder()
     parser = argparse.ArgumentParser()
     parser.add_argument("-p")
     args = parser.parse_args()
